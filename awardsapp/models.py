0dbee51b107e5902fb0cb95266b0312eefcf7b63
kabagemark <kabagemark@gmail.com>
2018-10-13 11:22:19 +0300

    project model added

--- awardsapp/models.py
+++ awardsapp/models.py
@@ -1,3 +1,78 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to='profile-pics')
+    bio = models.TextField()
+    user= models.ForeignKey(User)
+
+    @classmethod
+    def get_all(cls):
+        all_objects = Profile.objects.all()
+        return all_objects
+     
+    @classmethod
+    def search_by_username(cls,search_term):
+        profile = cls.objects.filter(user__name__icontains=search_term)
+        return profile
+    @classmethod
+    def update_caption(cls,current_value,new_value):
+        fetched_object = Image.objects.filter(name=current_value).update(name=new_value)
+        return fetched_object
+
+    @classmethod
+    def save_profile(self):
+        return self.save()
+
+    @classmethod   
+    def delete_image(self):
+        return self.delete()
+
+
+class Project(models.Model):
+    project_image = models.ImageField(upload_to='projects-images')
+    project_title = models.CharField(max_length =30)
+    project_description = models.CharField(max_length =30)
+    project_link = models.URLField()
+    image_comments = models.TextField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE ,null=True)
+    
+    @classmethod
+    def get_all(cls):
+        all_objects = Image.objects.all()
+        return all_objects
+
+    @classmethod
+    def get_image_by_id(cls,incoming_id):
+        image_result = cls.objects.get(id=incoming_id)
+        return image_result
+
+    @classmethod
+    def update_caption(cls,current_value,new_value):
+        fetched_object = Image.objects.filter(name=current_value).update(name=new_value)
+        return fetched_object
+
+    @classmethod
+    def save_image(self):
+        return self.save()
+
+    @classmethod    
+    def delete_image(self):
+        return self.delete()
+ 
+class Comment(models.Model):
+    user = models.ForeignKey(User,null=True)
+    post=models.ForeignKey(Image,related_name='comments',null=True)
+    comment=models.CharField(max_length=200,null=True)
+
+    def __str__(self):
+        return self.comment
+
+class Likes(models.Model):
+    user = models.OneToOneField(User,related_name='l_user')
+    post=models.ForeignKey(Image,related_name='likes')
+    like=models.CharField(max_length=3,default='1')
+
+
