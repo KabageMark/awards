93b022b4dc5db6b5530ff9aac9f529c8206123bb
kabagemark <kabagemark@gmail.com>
2018-10-13 13:07:04 +0300

    app conected to postgress

--- awardsproject/settings.py
+++ awardsproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'psycopg2',
     'bootstrap4',
     'awardsapp.apps.AwardsappConfig',
     'django.contrib.admin',
@@ -77,11 +78,12 @@ WSGI_APPLICATION = 'awardsproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'awards',
+        'USER': 'markchege',
+    'PASSWORD':'konshens',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
