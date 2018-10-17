ce78687d55e1624171ab6501184dfd0d95e78107
kabagemark <kabagemark@gmail.com>
2018-10-17 17:43:31 +0300

    post updates url added

--- awardsapp/urls.py
+++ awardsapp/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     url('^$',views.home,name = 'home'),
     url('^post$',views.NewPost,name = 'post-project'),
+     url('^update$',views.NewProfile,name = 'post-update'),
     url(r'^review/(\d+)', views.NewReview, name='review'),
     url('^search$',views.search_results,name = 'search-projects'),
     
